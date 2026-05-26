42ccc76152457fec6846e2c818b28307557ceae0
Raoshan <raoshankumarsaw@gmail.com>
2026-05-26 13:22:28 +0530

    fix selenium driver

--- scrapers/icontainers.py
+++ scrapers/icontainers.py
@@ -49,6 +49,11 @@ data = []
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 options = Options()
 
 options.binary_location = "/usr/bin/chromium"
@@ -58,10 +63,8 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 
-service = Service("/usr/bin/chromedriver")
-
 driver = webdriver.Chrome(
-    service=service,
+    service=Service(ChromeDriverManager().install()),
     options=options
 )
 # =========================================================
